66244a0b5b61957d54362c93b23460c061dd24d5
Cyntra <140875968+Cyntra-22@users.noreply.github.com>
2025-02-25 21:41:55 +0700

    Added Login and Sign Up pages

---
+++ art-platform/src/routes/login/+page.svelte
@@ -0,0 +1,115 @@
+<script lang="ts">
+</script>
+<style>
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin: 12rem auto;
+    }
+
+
+    .login-box {
+        background-color: #fff;
+        padding: 40px;
+        border-radius: 8px;
+        box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+        width: 350px;
+        text-align: center;
+    }
+
+    .login-box h2 {
+        margin-bottom: 1rem;
+        color: #333;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        margin-bottom: 20px;
+    }
+
+    label {
+        font-size: 1rem;
+        color: #555;
+        text-align: left;
+    }
+
+    input {
+        padding: 10px;
+        font-size: 0.8rem;
+        border: 1px solid #ddd;
+        border-radius: 2rem;
+        background-color: #f9f9f9;
+        margin-bottom: 0.3rem;
+    }
+
+    button {
+        padding: 12px;
+        border: none;
+        border-radius: 2rem;
+        font-size: 1rem;
+        cursor: pointer;
+        color: #fff;
+        background-color: hsl(5, 85%, 63%);
+        margin-top: 10px;
+    }
+
+    button:hover {
+        background-color: hsl(5, 67%, 52%);
+    }
+
+
+
+    .forgot-password a {
+        font-size: 0.85rem;
+        color: hsl(5, 85%, 63%);
+        text-decoration: none;
+        float: left;
+    }
+
+
+    .forgot-password1 a{
+        font-size: 0.85rem;
+        color: hsl(5, 85%, 63%);
+        text-decoration: none;
+    }
+
+
+    .forgot-password a:hover {
+        text-decoration: underline;
+    }
+    .login-box p {
+      
+        color: #666;
+        font-size: 0.9rem;
+    }
+
+
+</style>
+
+<div class="container">
+   
+    <div class="login-box">
+        <h2>Welcome to Art World</h2>
+        <form >
+            <label for="email">Email</label>
+            <input type="email" id="email" placeholder="Email" required>
+
+            <label for="password">Password</label>
+            <input type="password" id="password" placeholder="********" required>
+
+            <div class="forgot-password">
+                <a href="/">Forgot your password?</a>
+            </div>
+
+            <button type="submit">Log in</button>
+            <p class="forgot-password1">Not Account Yet? <a href="/login" style="font-size:small">Sign up </a></p>
+        </form>
+        
+        
+
+    </div>
+</div>
---
+++ art-platform/src/routes/signup/+page.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    
+</script>
+<style>
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        max-width: 400px;
+        margin: 5rem auto;
+    }
+
+
+    .signup-box {
+        background-color: #fff;
+        padding: 40px;
+        border-radius: 8px;
+        box-shadow: 0 4px 12px rgba(55, 17, 17, 0.1);
+        text-align: center;
+        width: 100%;
+    }
+
+    .signup-box h2 {
+        font-size: 1.5rem;
+        margin-bottom: 1rem;
+        color: #333;
+    }
+
+    .signup-box p {
+        margin-bottom: 28px;
+        color: #666;
+        font-size: 0.9rem;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    label {
+        font-size: 1rem;
+        color: #555;
+        text-align: left;
+        margin-bottom: 0.3rem;
+    }
+
+    input {
+        padding: 12px;
+        font-size: 0.8rem;
+        border: 1px solid #ddd;
+        border-radius: 2rem;
+        background-color: #f9f9f9;
+        margin-bottom: 1rem;
+    }
+
+    input:focus {
+        outline: none;
+        border-color: hsl(5, 85%, 63%);
+    }
+
+   
+
+    .signup-btn {
+        padding: 12px;
+        background-color: hsl(5, 85%, 63%);
+        color: white;
+        border: none;
+        border-radius: 2rem;
+        cursor: pointer;
+        font-size: 1rem;
+        margin-top: 15px;
+        
+        
+    }
+
+    .signup-btn:hover {
+        background-color: hsl(5, 80%, 57%);
+    }
+
+    a{
+        text-decoration: none;
+        color:hsl(5, 80%, 57%);
+    }
+
+
+</style>
+
+<div class="container">
+
+    <div class="signup-box">
+        <h2>Welcome to Art World</h2>
+        <form >
+
+            <label for="email">Email</label>
+            <input type="email" id="email" placeholder="myopa@gmail.com" required>
+
+            <label for="confirm-email"> Confirm Email</label>
+            <input type="email" id="email" placeholder="Confirm your email" required>
+
+            <label for="password">Password</label>
+            <input type="password" id="password" placeholder="Create a password" required>
+
+            <label for="confirm-password">Confirm Password</label>
+            <input type="password" id="password" placeholder="Confirm password" required>
+
+            <label for="birthday">Birthday</label>
+            <input type="date" id="birthday"  required>
+
+            <button type="submit" class="signup-btn">Continue</button>
+        </form>
+        <div class="forgot-password">
+            <p>Already have an account? <a href="/login" style="font-size:small">Log in </a></p>
+        </div>
+
+    </div>
+</div>
