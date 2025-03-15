9de5029f8d758b9a05525e6e6745410a5468ed36
HariTTira <haritpao@gmail.com>
2025-03-15 21:09:47 +0700

    set up database

--- art-server/index.js
+++ art-server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv').config();
+const {mongoose} = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI;
+
+//database connection
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Database Connected'))
+  .catch((err) => console.error('Database Connection Error:', err));
 
 //middleware
 app.use(express.json())
