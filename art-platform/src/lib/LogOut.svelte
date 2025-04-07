beccf645959e28a58e7f79b01c0bfcf413db2349
shinyyxxx <puriwat888@gmail.com>
2025-04-07 22:07:00 +0700

    feat: logout functionality

--- art-platform/src/lib/LogOut.svelte
+++ art-platform/src/lib/LogOut.svelte
@@ -1,12 +1,42 @@
 <script lang="ts">
+    import { userInfo } from "$lib/loginUserInfo";
 
+    function cancelLogout() {
+        // Go back to previous page or home page
+
+        history.back();
+    }
+
+    function handleLogout() {
+        // Clear token from localStorage
+        localStorage.removeItem("token");
+        
+        userInfo.set({
+            id: null,
+            name: "",
+            email: "",
+            isLoggedIn: false,
+        });
+
+        window.location.href = "/login";
+    }
 </script>
 
+<div class="logout-container">
+    <div class="logout-box">
+        <h2>Are you sure want to log out?</h2>
+        <form>
+            <button class="pbtn-2" onclick={cancelLogout}>Cancel</button>
+            <button class="pbtn-1" onclick={handleLogout}>Log Out</button>
+        </form>
+    </div>
+</div>
+
 <style>
-    .logout-container{
+    .logout-container {
         padding-top: 5rem;
     }
-    .logout-box{
+    .logout-box {
         width: 50%;
         margin: 0 auto;
         padding: 2rem;
@@ -15,7 +45,7 @@
         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     }
 
-    .pbtn-2{
+    .pbtn-2 {
         background-color: hsl(5, 85%, 63%);
         color: white;
         border: none;
@@ -26,12 +56,11 @@
         font-size: 0.8rem;
     }
 
-    .pbtn-2:hover{
+    .pbtn-2:hover {
         background-color: hsl(5, 85%, 50%);
-        
     }
 
-    .pbtn-1{
+    .pbtn-1 {
         background-color: white;
         color: hsl(5, 85%, 63%);
         border: none;
@@ -43,18 +72,8 @@
         font-size: 0.8rem;
     }
 
-    .pbtn-1:hover{
+    .pbtn-1:hover {
         color: hsl(5, 85%, 50%);
         border: 1px solid hsl(5, 85%, 50%);
     }
 </style>
-
-<div class="logout-container">
-    <div class="logout-box">
-        <h2>Are you sure want to log out?</h2>
-        <form>
-            <button class="pbtn-2">Cancel</button>
-            <button class="pbtn-1">Log Out</button>
-        </form>
-    </div>
-</div>
